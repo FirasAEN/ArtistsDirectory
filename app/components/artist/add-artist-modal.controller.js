cb85eceb6309ab3da432b6990c5d24d2831864e8
FAB <noOne@noWhere>
2018-03-20 18:56:34 +0100

    improves 'add new artist' modal view
    Signed-off-by: FAB <noOne@noWhere>

--- app/components/artist/add-artist-modal.controller.js
+++ app/components/artist/add-artist-modal.controller.js
@@ -7,10 +7,24 @@
         .controller('ModalController', ModalController);
 
     ModalController.$inject = [
+        '$sce',
         '$mdDialog'
     ];
 
-    function ModalController($mdDialog){
+    function ModalController($sce,
+                             $mdDialog){
+        var vm = this;
+        vm.tooltip = $sce.trustAsHtml("tooltip");
 
+        vm.accept = accept;
+        vm.cancel = cancel;
+
+        function accept(){
+            $mdDialog.hide();
+        }
+
+        function cancel() {
+            $mdDialog.cancel();
+        }
     }
 })();
