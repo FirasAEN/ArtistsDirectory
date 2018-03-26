b3d7889a61fbc84e53f1d7e82fae350a71709d52
FAB <noOne@noWhere>
2018-03-26 10:28:01 +0200

    Dynamically adds classes to elements on construction and destruction
    Signed-off-by: FAB <noOne@noWhere>

--- app/components/address-book/address-book-toaster.controller.js
+++ app/components/address-book/address-book-toaster.controller.js
@@ -7,28 +7,45 @@
         .controller('AddressBookToasterController', AddressBookToasterController);
 
     AddressBookToasterController.$inject = [
+        '$scope',
         '$element',
         'ToastService',
         'toaster'];
 
-    function AddressBookToasterController($element, ToastService, toaster) {
+    function AddressBookToasterController($scope, $element, ToastService, toaster) {
         let vm = this;
         vm.text = toaster.text;
         vm.hide = closeToaster;
 
-        init();
-        function init(){
+        onInit();
+        onDestroy();
+
+        function onInit(){
             // $element.addClass(vm.style);
-            // angular.element(_addClass);
+            angular.element(_addClass);
         }
 
         function _addClass(){
-            angular.element('#addressBookToaster')
-                .addClass(vm.style);
+            // angular.element('#toasterContainer')
+            //     .addClass('md-toaster-fixed-top-visible');
+
+            // angular.element('#sideNavTrigger')
+            //     .addClass('elevate');
         }
 
         function closeToaster(){
             ToastService.hide();
+        }
+
+
+        function onDestroy(){
+            $scope.$on('$destroy', function(){
+                // angular.element('#toasterContainer')
+                //     .removeClass('md-toaster-fixed-top-visible');
+
+                // angular.element('#sideNavTrigger')
+                //     .removeClass('elevate');
+            });
         }
     }
 })();
